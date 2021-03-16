9703f5c10ee1116d09e5431734d7e312cab86efd
Molly Huang <molly.h@gmail.com>
2021-03-16 22:52:45 +0800

    #2 - 14:39​ - Fetching data - posts

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // javascript for index.html
+const container = document.querySelector('.blogs');
+// console.log('container==>', container)
+
+const rendrerPosts = async () => {
+  let url = 'http://localhost:3000/posts';
+
+  const res = await fetch(url);
+  const posts = await res.json();
+  // console.log('posts==>', posts)
+
+  let template = '';
+  posts.forEach(post => {
+    template += `
+      <div class="post">
+        <h2>${post.title}</h2>
+        <p><small>${post.likes}</small></p>
+        <p>${post.body.slice(0, 200)}</p>
+        <a href="/details.html">read more...</a>
+      </div>
+    `
+  });
+
+  container.innerHTML = template;
+}
+
+window.addEventListener('DOMContentLoaded', () => rendrerPosts());
